4a4dffaa9c774c74ce23fe6994bc12c41a8f972e
Sakshi Jagtap <99532147+sakshikjagtap@users.noreply.github.com>
2022-06-06 17:07:40 +0530

    Stored data into json file

--- form.js
+++ form.js
@@ -1,19 +1,63 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 
-const getInput = function () {
-  const details = {};
+class Form {
+  #queries;
+  #details;
+  constructor(queries) {
+    this.#queries = queries;
+    this.#details = { name: null, DOB: null, Hobbies: [] };
+  }
+
+  getQueries() {
+    return this.#queries;
+  }
+
+  getKeys() {
+    return Object.keys(this.#details);
+  }
+
+  setValues(key, value) {
+    this.#details[key] = value;
+  }
+
+  write() {
+    fs.writeFileSync('./form.json', JSON.stringify(this.#details));
+  }
+
+  name(name) {
+    const reg = /[a-z]{4}/;
+    reg.test(name);
+  }
+}
+
+const input = (form) => {
+  const query = form.getQueries();
+  const keys = form.getKeys();
   process.stdin.setEncoding('utf8');
+  let count = 0;
+  console.log(query[count]);
   process.stdin.on('data', (chunk) => {
+    count++;
     let input = '';
     input += chunk;
     const line = input.split('\n');
-    details.name = line.slice(0, -1);
+    console.log(query[count]);
+    form.setValues(keys[count - 1], line.slice(0, -1));
     input = line.slice(-1);
+    if (count === 3) {
+      form.write();
+      process.exit();
+    }
   });
+
 };
 
-const write = (data) => fs.writeFileSync('./form.json', data);
+const query = [
+  'Please Enter the name',
+  'Please enter the DOB',
+  'Please Enter the hobbies'
+];
 
-console.log('Enter name:');
-getInput(write);
-
+const form = new Form(query);
+input(form);
